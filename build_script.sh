df4c61322909d9c68040dd644b7b81da648e9a94
Rachit Singh <rachitsingh@outlook.com>
2017-12-09 18:05:46 -0500

    .

--- build_script.sh
+++ build_script.sh
@@ -1,5 +1,5 @@
 rm -rf public
-git worktree add -B gh-pages public upstream/gh-pages
+git worktree add -B master public upstream/master
 echo "rachitsingh.com" >> public/CNAME
 hugo
-cd public && git add --all && git commit -m "Publishing to gh-pages" && cd ..
+cd public && git add --all && git commit -m "." && cd ..
