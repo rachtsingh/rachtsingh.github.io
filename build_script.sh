b37c3b49abcd599d511e179f81f19f1073baee61
Rachit Singh <rachitsingh@outlook.com>
2017-12-09 18:06:42 -0500

    .

--- build_script.sh
+++ build_script.sh
@@ -1,5 +1,3 @@
-rm -rf public
-git worktree add -B master public upstream/master
-echo "rachitsingh.com" >> public/CNAME
 hugo
+echo "rachitsingh.com" > public/CNAME
 cd public && git add --all && git commit -m "." && cd ..
