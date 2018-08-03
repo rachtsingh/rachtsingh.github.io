b087a69ba85c9fe65ddef0b4f5be4f641d86d58f
Rachit Singh <rachitsingh@outlook.com>
2018-08-03 16:49:46 -0700

    Edited the build script to add a new echo

--- build_script.sh
+++ build_script.sh
@@ -2,6 +2,8 @@ rm -rf public
 git worktree prune
 git worktree add -B master public/ upstream/master
 
+echo "saying something here"
+
 # grab and rebuild the comments directory
 python grab_comments.py
 
